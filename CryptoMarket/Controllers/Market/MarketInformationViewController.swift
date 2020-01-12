6f7299fef27ee8260c67eca4e3d2550a9ceb24ca
Thomas Martins <thomas.martins@epitech.eu>
2020-01-12 20:25:43 +0100

    clean

--- CryptoMarket/Controllers/Market/MarketInformationViewController.swift
+++ CryptoMarket/Controllers/Market/MarketInformationViewController.swift
@@ -217,31 +217,3 @@ extension MarketInformationViewController: UITableViewDelegate, UITableViewDataS
         }
     }
 }
-
-
-public protocol NibInstantiatable {
-
-    static func nibName() -> String
-
-}
-
-extension NibInstantiatable {
-
-    static func nibName() -> String {
-        return String(describing: self)
-    }
-
-}
-
-extension NibInstantiatable where Self: UIView {
-
-    static func fromNib() -> Self {
-
-        let bundle = Bundle(for: self)
-        let nib = bundle.loadNibNamed(nibName(), owner: self, options: nil)
-
-        return nib!.first as! Self
-
-    }
-
-}
