4b4ab6b4cc8fa25fc6e0b3df97338ea038eec115
Thomas Martins <thomas.martins@epitech.eu>
2020-01-16 21:17:58 +0100

    Starting the feed news UI again 🔥

--- CryptoMarket/Controllers/MarketNews/MarketNewsViewController.swift
+++ CryptoMarket/Controllers/MarketNews/MarketNewsViewController.swift
@@ -15,12 +15,12 @@ class MarketNewsViewController: UIViewController {
     //MARK: Members
     private let viewModel: MarketNewsViewModel = MarketNewsViewModel()
     private let disposeBag = DisposeBag()
-    private var collectionViewDataSource: [MarketNews] = []
-    private let collectionSpinner = UIActivityIndicatorView(style: .whiteLarge)
+    private var tableViewDataSource: [MarketNews] = []
+    private let tableViewSpinner = UIActivityIndicatorView(style: .whiteLarge)
     private let refreshControl = UIRefreshControl()
     
     //MARK: Outlets
-    @IBOutlet private weak var collectionViewNews: UICollectionView!
+    @IBOutlet private weak var tableViewNews: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,19 +30,18 @@ class MarketNewsViewController: UIViewController {
     }
     
     private func setupView() {
-        self.collectionViewNews.register(MarketNewCell.nib, forCellWithReuseIdentifier: MarketNewCell.identifier)
-        self.collectionViewNews.delegate = self
-        self.collectionViewNews.dataSource = self
+        self.tableViewNews.register(NewsTableViewCell.nib, forCellReuseIdentifier: NewsTableViewCell.identifier)
+        self.tableViewNews.delegate = self
+        self.tableViewNews.dataSource = self
     
-        self.view.addSubview(self.collectionSpinner)
-        self.collectionSpinner.translatesAutoresizingMaskIntoConstraints = false
-        self.collectionSpinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        self.collectionSpinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        self.collectionSpinner.isHidden = false
-        self.collectionSpinner.startAnimating()
+        self.view.addSubview(self.tableViewSpinner)
+        self.tableViewSpinner.translatesAutoresizingMaskIntoConstraints = false
+        self.tableViewSpinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        self.tableViewSpinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        self.tableViewSpinner.isHidden = false
+        self.tableViewSpinner.startAnimating()
         self.refreshControl.tintColor = UIColor.init(named: "White")
-        self.collectionViewNews.refreshControl = refreshControl
-        
+        self.tableViewNews.refreshControl = refreshControl
         self.extendedLayoutIncludesOpaqueBars = true
         self.navigationItem.title = "News"
         self.navigationController?.navigationBar.barTintColor = UIColor.init(named: "MainColor")
@@ -61,10 +60,12 @@ class MarketNewsViewController: UIViewController {
         output.collectionViewDataSource
             .observeOn(MainScheduler.instance)
             .subscribeOn(MainScheduler.asyncInstance)
-            .subscribe(onNext: { (collectionViewDataSource) in
-                self.collectionViewDataSource = collectionViewDataSource
-                self.collectionViewNews.reloadData()
-                self.collectionViewNews.refreshControl?.endRefreshing()
+            .subscribe(onNext: { (tableViewDataSource) in
+                self.tableViewDataSource = tableViewDataSource
+                self.tableViewNews.reloadData()
+                
+                //todo remove this
+                self.tableViewNews.refreshControl?.endRefreshing()
             }, onError: { (error) in
                 self.handleErrorOnRetry(error: error, message: ErrorMessage.errorMessageNews) {
                     self.setupViewModel()
@@ -76,53 +77,75 @@ class MarketNewsViewController: UIViewController {
             .observeOn(MainScheduler.instance)
             .subscribeOn(MainScheduler.asyncInstance)
             .subscribe(onNext: { (isLoading) in
-                self.collectionViewNews.isHidden = isLoading
-                self.collectionSpinner.isHidden = !isLoading
-                isLoading ? self.collectionSpinner.startAnimating() : self.collectionSpinner.stopAnimating()
+                self.tableViewNews.isHidden = isLoading
+                //todo change this name
+                self.tableViewSpinner.isHidden = !isLoading
+                isLoading ? self.tableViewSpinner.startAnimating() : self.tableViewSpinner.stopAnimating()
             }).disposed(by: self.disposeBag)
     }
 }
 
-extension MarketNewsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 20
+extension MarketNewsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableViewDataSource.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (self.view.frame.width - 3) / 1
-        return CGSize(width: width, height: width)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.collectionViewDataSource.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MarketNewCell.identifier, for: indexPath) as? MarketNewCell {
-            cell.title = self.collectionViewDataSource[indexPath.row].title
-            cell.content = self.collectionViewDataSource[indexPath.row].content
-            cell.loadImageOnCell(urlImage: self.collectionViewDataSource[indexPath.row].urlToImage ?? "")
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: NewsTableViewCell.identifier, for: indexPath) as? NewsTableViewCell {
+            cell.title = self.tableViewDataSource[indexPath.row].title
+            cell.date = self.tableViewDataSource[indexPath.row].publishedAt
+            cell.loadImageOnCell(urlImage: self.tableViewDataSource[indexPath.row].urlToImage)
             return cell
         }
-        return UICollectionViewCell()
+        return UITableViewCell()
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let imageUrl = self.collectionViewDataSource[indexPath.row].url else { return }
-        if let url = URL(string: imageUrl) {
-            let config = SFSafariViewController.Configuration()
-            
-            config.entersReaderIfAvailable = true
-            let vc = SFSafariViewController(url: url, configuration: config)
-            vc.preferredBarTintColor = UIColor.black
-            vc.preferredControlTintColor = UIColor.systemBlue
-            
-            self.present(vc, animated: true)
-        }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
     }
+    
 }
+
+//extension MarketNewsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 20
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 10
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let width = (self.view.frame.width - 3) / 1
+//        return CGSize(width: width, height: width)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return self.collectionViewDataSource.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MarketNewCell.identifier, for: indexPath) as? MarketNewCell {
+//            cell.title = self.collectionViewDataSource[indexPath.row].title
+//            cell.content = self.collectionViewDataSource[indexPath.row].content
+//            cell.loadImageOnCell(urlImage: self.collectionViewDataSource[indexPath.row].urlToImage ?? "")
+//            return cell
+//        }
+//        return UICollectionViewCell()
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        guard let imageUrl = self.collectionViewDataSource[indexPath.row].url else { return }
+//        if let url = URL(string: imageUrl) {
+//            let config = SFSafariViewController.Configuration()
+//
+//            config.entersReaderIfAvailable = true
+//            let vc = SFSafariViewController(url: url, configuration: config)
+//            vc.preferredBarTintColor = UIColor.black
+//            vc.preferredControlTintColor = UIColor.systemBlue
+//
+//            self.present(vc, animated: true)
+//        }
+//    }
+//}
---
+++ CryptoMarket/Scenes/MarketNews/Cell/NewsTableViewCell.swift
@@ -0,0 +1,95 @@
+//
+//  NewsTableViewCell.swift
+//  CryptoMarket
+//
+//  Created by Thomas Martins on 16/01/2020.
+//  Copyright © 2020 Thomas Martins. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class NewsTableViewCell: UITableViewCell {
+
+    //MARK: Outlets
+    @IBOutlet private weak var titleNews: UILabel!
+    @IBOutlet private weak var dateLabelNews: UILabel!
+    @IBOutlet private weak var imageNews: UIImageView!
+    
+    //MARK: Members
+    private let viewModel: MarketNewsCellViewModel = MarketNewsCellViewModel()
+    private let disposeBag = DisposeBag()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.setupView()
+    }
+    
+    private func setupView() {
+        self.imageNews.layer.cornerRadius = 10
+    }
+    
+    public var title: String? {
+        get {
+            return self.titleNews.text
+        }
+        set {
+            self.titleNews.text = newValue
+        }
+    }
+    
+    public var date: String? {
+        get {
+            return self.dateLabelNews.text
+        }
+        set {
+            self.dateLabelNews.text = newValue
+        }
+    }
+    
+    public func loadImageOnCell(urlImage: String?) {
+        
+        //MARK: put a default picture if none
+        guard let urlImage = urlImage, !urlImage.isEmpty else { return }
+        
+        let input = MarketNewsCellViewModel.Input(imageName: Driver.just(urlImage))
+        let output = self.viewModel.transform(input: input)
+              
+        self.viewModel.currentDownloadUrl = urlImage
+              
+        output.imageDownloaded.asObservable()
+            .subscribeOn(MainScheduler.asyncInstance)
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { (imageView) in
+                guard let image = imageView else { return }
+                    if self.viewModel.currentDownloadUrl == urlImage {
+                        self.imageNews.image = image
+                    }
+            }).disposed(by: self.disposeBag)
+              
+        output.isImageLoading.asObservable()
+            .subscribeOn(MainScheduler.asyncInstance)
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { (isLoading) in
+                self.imageNews.isHidden = isLoading
+            }).disposed(by: self.disposeBag)
+    }
+    
+    private func setupViewModel() {
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static var nib: UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+}
