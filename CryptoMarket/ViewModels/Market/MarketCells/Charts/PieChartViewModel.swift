4d7a81ede695438a349c88840dae6445dbe53911
Thomas Martins <thomas.martins@epitech.eu>
2020-03-16 00:20:50 +0100

    [ADD] display data on view and color chart

--- CryptoMarket/ViewModels/Market/MarketCells/Charts/PieChartViewModel.swift
+++ CryptoMarket/ViewModels/Market/MarketCells/Charts/PieChartViewModel.swift
@@ -15,23 +15,53 @@ internal final class PieChartViewModel: ViewModelType {
     
     private let disposeBag: DisposeBag = DisposeBag()
     
-    struct Input {
-        
-    }
+    struct Input { }
     
     struct Output {
-        
+        let marketEmotion: Observable<[MarketEmotion]>
+        let pieViewData: Observable<([PieChartDataEntry], UIColor)>
     }
     
     private func fetchFeerAndGreed() -> Observable<[MarketEmotion]> {
         return Network.sharedInstance.performGetOnFeerAndGred(string: ApiRoute.ROUTE_SERVER_FEER.concat(string: ApiRoute.ROUTE_FEER_GREED))
     }
     
-    func transform(input: Input) -> Output {
-        let res = self.fetchFeerAndGreed().subscribe(onNext: { (result) in
-            print("[TRANSFORM] = \(result)")
-        }).disposed(by: self.disposeBag)
+    private func fetchDataEntries() -> Observable<[PieChartDataEntry]> {
         
-        return Output()
+        return self.fetchFeerAndGreed().asObservable()
+            .observeOn(MainScheduler.instance)
+            .subscribeOn(MainScheduler.asyncInstance)
+            .map { (marketInformation) -> [PieChartDataEntry] in
+                var result: [PieChartDataEntry] = []
+                for element in marketInformation {
+                    result.append(PieChartDataEntry(value: Double(element.value ?? "0") ?? 0, label: "\(element.value_classification ?? "None")"))
+                }
+                
+                //Add default case for the pieChart x/100%
+                result.append(PieChartDataEntry(value: (100 - (result.last?.value ?? 0)), label: ""))
+                
+                return result
+            }
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        let chartData: Observable<[PieChartDataEntry]> = self.fetchDataEntries()
+        
+        
+        let chartResult: Observable<([PieChartDataEntry], UIColor)> = chartData.map { (data) -> ([PieChartDataEntry], UIColor) in
+            switch (data.first?.value ?? 0) {
+            case let fear where fear <= 30:
+                return (data, UIColor.init(named: "SortDown") ?? UIColor.white)
+            case let fear where fear > 30 && fear < 70:
+                return (data, UIColor.init(named: "SecondaryColor") ?? UIColor.white)
+            case let fear where fear >= 70:
+                return (data, UIColor.init(named: "SortUp") ?? UIColor.white)
+            default:
+                return (data, UIColor.white)
+            }
+        }
+        
+        return Output(marketEmotion: self.fetchFeerAndGreed(), pieViewData: chartResult)
     }
 }
